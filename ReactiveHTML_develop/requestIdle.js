39fc17c925a71076df1d0ba95c383463972d3c62
Ludvík Prokopec <39367469+ujikCz@users.noreply.github.com>
2021-01-23 23:10:45 +0100

    Add files via upload
    polyfill for requestIdleCallback

--- ReactiveHTML_develop/requestIdle.js
+++ ReactiveHTML_develop/requestIdle.js
@@ -1,13 +1,23 @@
 
+window.requestIdleCallback = window.requestIdleCallback || function(callback) {
+
+    window.requestAnimationFrame(function() {
+        callback({
+            timeRemaining: () => 0
+        });
+    });
+
+} //simple polyfill for IE10-IE11
 
 export default function requestIdle(callback) {
     
-    const deadline = Date.now();
+    const deadline = performance.now();
     
     function schedule() {
 
         window.requestIdleCallback(idleData => {
-            const now = Date.now();
+
+            const now = performance.now();
 
             if (idleData.timeRemaining() > 1 || now >= deadline) {
 
